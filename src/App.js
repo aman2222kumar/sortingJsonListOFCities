b6b1ea2b803c3558f0edbda5e94946182e60c341
aman2222kumar <amanda.asd444@gmail.com>
2022-10-18 10:19:04 +0530

    firstAdded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from "react";
+
+
 function App() {
+  const [titsbig, setTitsBig] = useState([]);
+  // const [country, setcountry] = useState();
+
+  // function handlechane(e) {
+  //   setcountry(e.target.value);
+  // }
+
+  useEffect(() => {
+    fetch("https://countriesnow.space/api/v0.1/countries/population/cities")
+      .then((res) => res.json())
+      .then((data) => {
+        data.data.sort((a, b) => a.city.localeCompare(b.city));
+        setTitsBig(data.data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        {titsbig.map((v, index) => {
+          return <option key={index}>{v.city}</option>;
+        })}
+      </select>
+      {/* <input type="text" value={country} onChange={handlechane}></input>
+      <div>
+        {titsbig.map((r) => {
+          if (country === "") {
+            return country;
+          } else if (r.city.includes(country)) {
+            return r.city;
+          }
+        })}
+      </div> */}
     </div>
   );
 }
